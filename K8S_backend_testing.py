57f3f4191ef213deebfe29c84a8b1b921d1083f1
photop33 <75907575+photop33@users.noreply.github.com>
2021-03-08 14:35:23 +0200

    Update K8S_backend_testing.py

--- K8S_backend_testing.py
+++ K8S_backend_testing.py
@@ -7,6 +7,12 @@ read = dic.read()
 print (read)
 
 
+
+res = requests.get('+ read +' + user_id + '')
+if res.ok:
+    print(res.json())
+
+    
 res = requests.get('http://127.0.0.1:5500/user/' + user_id + '')
 if res.ok:
     print(res.json())
